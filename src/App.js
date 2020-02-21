c58166cb950322d6eea1e88719eb962c1369b087
Ankit suthar <ankit.suthar@heliossolutions.in>
2020-02-21 19:18:51 +0530

    Add material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Button from '@material-ui/core/Button';
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	<Button variant="contained" color="primary">
+	      Hello World
+	</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
